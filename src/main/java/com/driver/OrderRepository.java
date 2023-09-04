58ad165454f6249b7304a8c7f181f878fc1734a9
Harsh Srivastava <srivastavaharsh8957@gmail.com>
2023-09-04 20:24:20 +0530

    first commit

---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,130 @@
+package com.driver;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class OrderRepository {
+
+    private HashMap<String , Order> orders;
+    private HashMap<String , DeliveryPartner> partners;
+    private HashMap<String, List<String>> partnerOrderHashMap;
+
+    private HashMap<String, String> orderPartnerHashMap;
+    private Integer orderAssignedToPartner;
+
+    public OrderRepository() {
+        orders = new HashMap<>();
+        partners = new HashMap<>();
+        partnerOrderHashMap = new HashMap<>();
+        orderAssignedToPartner = 0;
+        orderPartnerHashMap = new HashMap<>();
+    }
+    public void addOrder(Order order) {
+        orders.put(order.getId(), order);
+    }
+
+    public void addPartner(String partnerId) {
+
+        DeliveryPartner deliveryPartner = new DeliveryPartner(partnerId);
+        partnerOrderHashMap.put(partnerId, new ArrayList<>());
+        partners.put(partnerId, deliveryPartner);
+    }
+
+    public void addOrderPartnerPair(String orderId, String partnerId) {
+        partnerOrderHashMap.get(partnerId).add(orderId);
+        partners.get(partnerId).setNumberOfOrders(partners.get(partnerId).getNumberOfOrders()+1);
+
+        orderPartnerHashMap.put(orderId , partnerId);
+        orderAssignedToPartner++;
+
+    }
+
+    public Order getOrderById(String orderId) {
+        return orders.get(orderId);
+    }
+
+    public DeliveryPartner getPartnerById(String partnerId) {
+        return partners.get(partnerId);
+    }
+
+    public Integer getOrderCountByPartnerId(String partnerId) {
+        return partnerOrderHashMap.get(partnerId).size();
+    }
+
+    public List<String> getOrdersByPartnerId(String partnerId) {
+        return partnerOrderHashMap.get(partnerId);
+    }
+
+    public List<String> getAllOrders() {
+        return new ArrayList<>(orders.keySet());
+    }
+
+    public Integer getCountOfUnassignedOrders() {
+        return orders.size() - orderAssignedToPartner;
+    }
+
+
+    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
+
+        //DeliveryPartner partner = partners.get(partnerId);
+        List<String> orderIds = partnerOrderHashMap.get(partnerId);
+        Integer orderLeft = 0;
+
+        int t = Integer.parseInt(time.substring(0, 2))*60 + Integer.parseInt(time.substring(3));
+
+        for(String orderId : orderIds) {
+            if(orders.get(orderId).getDeliveryTime() > t) {
+                orderLeft++;
+            }
+        }
+
+        return orderLeft;
+    }
+
+    public void deletePartnerById(String partnerId) {
+        int numberOfOrder = partners.get(partnerId).getNumberOfOrders();
+        orderAssignedToPartner -= numberOfOrder;
+        partnerOrderHashMap.remove(partnerId);
+        partners.remove(partnerId);
+
+    }
+
+    public void deleteOrderById(String orderId) {
+
+        String partnerId = orderPartnerHashMap.get(orderId);
+        orderPartnerHashMap.remove(orderId);
+        List<String> orderList = partnerOrderHashMap.get(partnerId);
+        int idx = 0;
+        for(int i=0; i<orderList.size(); i++) {
+            if(orderList.get(i).equals(orderId)) {
+                idx = i;
+                break;
+            }
+        }
+        orderList.remove(idx);
+
+        partners.get(partnerId).setNumberOfOrders(partners.get(partnerId).getNumberOfOrders() - 1);
+        orderAssignedToPartner--;
+        orders.remove(orderId);
+    }
+
+
+    public int getLastDeliveryTimeByPartnerId(String partnerId) {
+        List<String> ordersId = partnerOrderHashMap.get(partnerId);
+        int time = 0;
+
+        for(String orderId : ordersId) {
+            int t = orders.get(orderId).getDeliveryTime();
+            if(time < t) {
+                time = t;
+            }
+        }
+
+        return time;
+    }
+}
---
+++ src/main/java/com/driver/OrderService.java
@@ -0,0 +1,91 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+    public void addOrder(Order order) {
+        orderRepository.addOrder(order);
+    }
+
+    public void addPartner(String partnerId) {
+        orderRepository.addPartner(partnerId);
+    }
+
+    public void addOrderPartnerPair(String orderId, String partnerId) {
+
+        orderRepository.addOrderPartnerPair(orderId, partnerId);
+    }
+
+
+    public Order getOrderById(String orderId) {
+        return orderRepository.getOrderById(orderId);
+    }
+
+    public DeliveryPartner getPartnerById(String partnerId) {
+
+        return orderRepository.getPartnerById(partnerId);
+    }
+
+    public Integer getOrderCountByPartnerId(String partnerId) {
+
+        return orderRepository.getOrderCountByPartnerId(partnerId);
+    }
+
+    public List<String> getOrdersByPartnerId(String partnerId) {
+        return orderRepository.getOrdersByPartnerId(partnerId);
+    }
+
+    public List<String> getAllOrders() {
+        return orderRepository.getAllOrders();
+    }
+
+    public Integer getCountOfUnassignedOrders() {
+
+        return orderRepository.getCountOfUnassignedOrders();
+    }
+
+
+    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
+
+        return getOrdersLeftAfterGivenTimeByPartnerId(time, partnerId);
+    }
+
+    public void deletePartnerById(String partnerId) {
+        orderRepository.deletePartnerById(partnerId);
+    }
+
+    public void deleteOrderById(String orderId) {
+        orderRepository.deleteOrderById(orderId);
+    }
+
+    public String getLastDeliveryTimeByPartnerId(String partnerId) {
+
+        int time = orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
+        String string = "";
+
+        int m = time%60;
+        int h = time/60;
+
+        if(h < 10) {
+            string += '0'+h;
+        }else {
+            string += h;
+        }
+        string += ':';
+
+        if(m < 10) {
+            string += '0'+m;
+        }else {
+            string += m;
+        }
+
+        return string;
+    }
+}
